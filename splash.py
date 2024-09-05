76b586b6ac46f2057924203421492f35d72277f2
David Oliva <david.oliva@perficient.com>
2024-09-05 16:43:13 -0600

    First commit

---
+++ splash.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template, request, send_file, jsonify
+
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("splash.html")
+
+
+if __name__ == "__main__":
+    app.run()
+    # app.run(host='0.0.0.0', port=5000)
